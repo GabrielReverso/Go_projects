7d19811a16c4b8b8ae4b6893f662f4813dc21124
GabrielReverso <reversogabrielpereira@gmail.com>
2023-09-14 20:29:18 -0300

    BinaryTree

--- DataStructure/Tree/Tree.go
+++ DataStructure/Tree/Tree.go
@@ -12,11 +12,13 @@ type Tree struct {
 	root *Node
 }
 
+//Create a tree
 func Tree_create() *Tree {
 	new_tree := &Tree{root: nil}
 	return new_tree
 }
 
+//Add a value in the tree
 func Tree_add(tree *Tree, value int) {
 	new_node := &Node{left: nil, data: value, right: nil}
 
@@ -45,15 +47,30 @@ func add_node(current *Node, value int, new_node *Node) {
 		if current.right == nil {
 			current.right = new_node
 		}
+	default:
+		if current.left == nil {
+			current.left = new_node
+		} else if current.right == nil {
+			current.right = new_node
+		} else {
+			add_node(current.left, value, new_node)
+			if current.left == nil {
+				current.left = new_node
+			}
+		}
 	}
 }
 
+//Print in pre-order
 func Tree_pre_order(tree *Tree) {
 	if tree.root == nil {
 		fmt.Println("Empty list")
 		return
 	}
+	fmt.Printf("Pre-order\n")
+	fmt.Printf("[ ")
 	pre_order(tree.root)
+	fmt.Printf("]\n\n")
 }
 
 func pre_order(current *Node) {
@@ -61,7 +78,49 @@ func pre_order(current *Node) {
 		return
 	}
 
-	fmt.Println(current.data)
+	fmt.Printf("%v ", current.data)
 	pre_order(current.left)
 	pre_order(current.right)
+}
+
+//Print in order
+func Tree_order(tree *Tree) {
+	if tree.root == nil {
+		fmt.Println("Empty list")
+		return
+	}
+	fmt.Printf("Order\n")
+	fmt.Printf("[ ")
+	order(tree.root)
+	fmt.Printf("]\n\n")
+}
+
+func order(current *Node) {
+	if current == nil {
+		return
+	}
+	order(current.left)
+	fmt.Printf("%v ", current.data)
+	order(current.right)
+}
+
+//Print in post-order
+func Tree_post_order(tree *Tree) {
+	if tree.root == nil {
+		fmt.Println("Empty list")
+		return
+	}
+	fmt.Printf("Post-order\n")
+	fmt.Printf("[ ")
+	post_order(tree.root)
+	fmt.Printf("]\n\n")
+}
+
+func post_order(current *Node) {
+	if current == nil {
+		return
+	}
+	post_order(current.left)
+	post_order(current.right)
+	fmt.Printf("%v ", current.data)
 }
--- main.go
+++ main.go
@@ -1,14 +1,31 @@
 package main
 
 import (
-	"fmt"
 	tree "module/DataStructure/Tree"
 )
 
 func main() {
-
-	fmt.Println("Hello")
-
 	arvore := tree.Tree_create()
+
+	/* tree.Tree_add(arvore, 4)
+	tree.Tree_add(arvore, 2)
+	tree.Tree_add(arvore, 1)
+	tree.Tree_add(arvore, 3)
+	tree.Tree_add(arvore, 5)
+	tree.Tree_add(arvore, 5)
+	tree.Tree_add(arvore, 6) */
+
+	tree.Tree_add(arvore, 12)
+	tree.Tree_add(arvore, 10)
+	tree.Tree_add(arvore, 8)
+	tree.Tree_add(arvore, 18)
+	tree.Tree_add(arvore, 42)
+	tree.Tree_add(arvore, 12)
+	tree.Tree_add(arvore, 4)
+	tree.Tree_add(arvore, 66)
+
+	tree.Tree_pre_order(arvore)
+	tree.Tree_order(arvore)
+	tree.Tree_post_order(arvore)
 
 }
